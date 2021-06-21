c73dd2c94783397b934bed825c4b858fc1caf336
Dobrica Jadzic <dobrica.jadzic@gmail.com>
2021-06-21 14:55:13 +0200

    Added suppliers and register

---
+++ TheShop/Suppliers/Supplier.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TheShop.Models;
+
+namespace TheShop.Suppliers
+{
+    public interface ISupplier
+    {
+        bool ArticleInInventory(int id);
+
+        Article GetArticle(int id);
+
+    }
+    public class Supplier1 : ISupplier
+    {
+
+        public List<Article> articles = new List<Article> { 
+            new Article { Id = 1, Name = "Article from supplier1", Price = 458 } };
+
+        public bool ArticleInInventory(int id)
+        {
+            return articles.Exists(x => x.Id == id);
+        }
+
+        public Article GetArticle(int id)
+        {
+            return articles.First(x => x.Id == id);
+        }
+    }
+
+    public class Supplier2 : ISupplier
+    {
+
+        private List<Article> articles = new List<Article> {
+            new Article { Id = 1, Name = "Article from supplier2", Price = 459 } };
+
+        public bool ArticleInInventory(int id)
+        {
+            return articles.Exists(x => x.Id == id);
+        }
+
+        public Article GetArticle(int id)
+        {
+            return articles.First(x => x.Id == id);
+        }
+    }
+
+    public class Supplier3 : ISupplier
+    {
+
+        private List<Article> articles = new List<Article> {
+            new Article { Id = 1, Name = "Article from supplier3", Price = 460 } };
+
+        public bool ArticleInInventory(int id)
+        {
+            return articles.Exists(x => x.Id == id);
+        }
+
+        public Article GetArticle(int id)
+        {
+            return articles.First(x => x.Id == id);
+        }
+    }
+}
---
+++ TheShop/Suppliers/SupplierRegister.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace TheShop.Suppliers
+{
+    public class SupplierRegister
+    {
+        public HashSet<ISupplier> Suppliers { get; }
+
+        SupplierRegister()
+        {
+            Suppliers.Add(new Supplier1());
+            Suppliers.Add(new Supplier2());
+            Suppliers.Add(new Supplier3());
+        }
+    }
+}
