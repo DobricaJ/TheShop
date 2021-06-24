28bc343fa3ecf02868e469b84107dc9ad17d40ab
Dobrica Jadzic <dobrica.jadzic@gmail.com>
2021-06-24 16:56:40 +0200

    Created new logic for Suppliers

--- TheShop/Suppliers/Supplier.cs
+++ TheShop/Suppliers/Supplier.cs
@@ -1,64 +1,14 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using TheShop.Models;
 
 namespace TheShop.Suppliers
 {
-    public interface ISupplier
+    public class Supplier : ISupplier
     {
-        bool ArticleInInventory(int id);
+        public int Id { get; set; }
 
-        Article GetArticle(int id);
+        public string Name { get; set; }
 
-    }
-    public class Supplier1 : ISupplier
-    {
-
-        public List<Article> articles = new List<Article> { 
-            new Article { Id = 1, Name = "Article from supplier1", Price = 458 } };
-
-        public bool ArticleInInventory(int id)
-        {
-            return articles.Exists(x => x.Id == id);
-        }
-
-        public Article GetArticle(int id)
-        {
-            return articles.First(x => x.Id == id);
-        }
-    }
-
-    public class Supplier2 : ISupplier
-    {
-
-        private List<Article> articles = new List<Article> {
-            new Article { Id = 1, Name = "Article from supplier2", Price = 459 } };
-
-        public bool ArticleInInventory(int id)
-        {
-            return articles.Exists(x => x.Id == id);
-        }
-
-        public Article GetArticle(int id)
-        {
-            return articles.First(x => x.Id == id);
-        }
-    }
-
-    public class Supplier3 : ISupplier
-    {
-
-        private List<Article> articles = new List<Article> {
-            new Article { Id = 1, Name = "Article from supplier3", Price = 460 } };
-
-        public bool ArticleInInventory(int id)
-        {
-            return articles.Exists(x => x.Id == id);
-        }
-
-        public Article GetArticle(int id)
-        {
-            return articles.First(x => x.Id == id);
-        }
+        public List<Article> Articles { get; set; }
     }
 }
